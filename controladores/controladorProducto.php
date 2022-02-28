99a77ee61cf1dab98a187e1376f3cb6ed2eb5c9a
SamuelOviedo <isaaccoviedo@gmail.com>
2022-02-28 15:44:52 -0600

    corrección controladores

--- controladores/controladorProducto.php
+++ controladores/controladorProducto.php
@@ -1,6 +1,7 @@
 <?php
-include_once('modelos/modeloProducto.php');
+
 function listar(){
+    include_once(dirname(dirname(__FILE__)).'modelos/modeloProducto.php');
     $modeloProducto = new Producto();
     return $modeloProducto->getProducto();
 }
